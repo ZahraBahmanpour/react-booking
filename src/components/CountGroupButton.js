12d181119afc74634d42909a9a441fe65f32af78
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-09-07 13:58:24 +0430

    replace order of buttons in count component

--- src/components/CountGroupButton.js
+++ src/components/CountGroupButton.js
@@ -13,11 +13,11 @@ const CountGroupButton = ({
       sx={{ alignItems: "center" }}
     >
       <div style={{ width: "40%", marginRight: 10 }}>{title}</div>
-      <Button onClick={incrementHandler}>+</Button>
-      <Button disabled>{value}</Button>
       <Button onClick={decrementHandler} disabled={value < 1}>
         -
       </Button>
+      <Button disabled>{value}</Button>
+      <Button onClick={incrementHandler}>+</Button>
     </ButtonGroup>
   );
 };
