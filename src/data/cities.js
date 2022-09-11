cbfd3b6ef3520a3c798ab8c10968f4740082da56
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-09-11 14:13:31 +0430

    Add more cities to dummy data

--- src/data/cities.js
+++ src/data/cities.js
@@ -3,6 +3,10 @@ const cities = [
   { text: "Tehran", value: "Tehran" },
   { text: "New York", value: "New York" },
   { text: "Paris", value: "Paris" },
+  { text: "Los Angeles", value: "Los Angeles" },
+  { text: "Amesterdam", value: "Amesterdam" },
+  { text: "Nantes", value: "Nantes" },
+  { text: "Baltic Sea", value: "Baltic Sea" },
 ];
 
 export default cities;
