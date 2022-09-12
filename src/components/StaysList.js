584ab2eb9bcbc64ae9962251d96311a90ebddf9a
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-09-12 13:51:16 +0430

    fix staysList component to fetch data

--- src/components/StaysList.js
+++ src/components/StaysList.js
@@ -1,22 +1,25 @@
 import { Box } from "@mui/material";
 import BasicCard from "./Card/Card";
 import Pagination from "../components/Pagination/Pagination";
+import { useEffect, useState } from "react";
 import { DEFAULT_PAGE_SIZE } from "../utils/utils";
-import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getStays } from "../redux/features/staysSlice";
 
 const StaysList = ({ stays }) => {
   const [page, setPage] = useState(1);
-  const paginatedStays = stays.slice(
-    DEFAULT_PAGE_SIZE * (page - 1),
-    page * DEFAULT_PAGE_SIZE
-  );
+  const dispatch = useDispatch();
+  const { totalStaysCount } = useSelector((state) => state.stays);
+  useEffect(() => {
+    dispatch(getStays(page));
+  }, [page, dispatch]);
   return (
     <Box sx={{ padding: 2, display: "flex", flexDirection: "column", gap: 5 }}>
-      {paginatedStays.map((stay) => (
+      {stays.map((stay) => (
         <BasicCard key={stay.id} {...stay} />
       ))}
       <Pagination
-        count={Math.ceil(stays.length / DEFAULT_PAGE_SIZE)}
+        count={Math.ceil(Number(totalStaysCount) / DEFAULT_PAGE_SIZE)}
         handleChange={(e, value) => setPage(value)}
       />
     </Box>
