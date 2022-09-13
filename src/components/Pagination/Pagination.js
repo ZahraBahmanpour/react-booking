a47066b9e9d5ecc2d8c32c0116e99058a003b2a3
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-09-13 22:13:57 +0430

    add size prop to pagination component

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,11 +1,12 @@
 import { Pagination as MuiPagination } from "@mui/material";
 
-const Pagination = ({ count, handleChange }) => {
+const Pagination = ({ count, handleChange, size }) => {
   return (
     <MuiPagination
       count={count}
       color="primary"
       onChange={handleChange}
+      size={size}
       sx={{ margin: "0 auto" }}
     />
   );
