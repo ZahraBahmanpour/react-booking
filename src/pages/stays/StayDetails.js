dbbcf594f1fe01f937923adc81bd180f4258508a
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-09-14 12:14:58 +0430

    add buttons to tay Details page

--- src/pages/stays/StayDetails.js
+++ src/pages/stays/StayDetails.js
@@ -1,10 +1,13 @@
+import { Button, Grid } from "@mui/material";
+import { Container } from "@mui/system";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import staysServices from "../../services/stays.services";
 
 const StayDetails = () => {
   const [stay, setStay] = useState([]);
   const { stayId } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchStay = async () => {
@@ -14,7 +17,30 @@ const StayDetails = () => {
     fetchStay();
   }, [stayId]);
 
-  return <div>{stay.name}</div>;
+  const handleBooking = (e) => {
+    e.preventDefault();
+  };
+
+  const handleBack = (e) => {
+    e.preventDefault();
+    navigate("/stays");
+  };
+
+  return (
+    <Container>
+      <Grid container spacing={3} sx={{ marginTop: 5 }}>
+        <Grid item xs={2} sm={4} md={12}>
+          {stay.name}
+        </Grid>
+        <Grid item xs={2} sm={4} md={12}>
+          <Button onClick={handleBack}>Back</Button>
+          <Button variant="contained" onClick={handleBooking}>
+            Book now!
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default StayDetails;
