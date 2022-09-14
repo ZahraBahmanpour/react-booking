6ff6bd4e2a91f4fdf54824e86f38605ea00da6c1
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-09-14 22:39:08 +0430

    add profile to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,36 @@
-import { AppBar, Box, IconButton, Toolbar, Tooltip } from "@mui/material";
+import {
+  AppBar,
+  Avatar,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
 import { IoIosBed } from "react-icons/io";
 import { TbPlaneInflight } from "react-icons/tb";
 import { NavLink } from "react-router-dom";
 
+const settings = ["Saved", "Logout"];
+
 const Navbar = () => {
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ backgroundColor: "#003580" }}>
-        <Toolbar>
+    <AppBar position="static" sx={{ backgroundColor: "#003580" }}>
+      <Toolbar>
+        <Box sx={{ flexGrow: 1 }}>
           <NavLink
             to="/stays"
             style={({ isActive }) =>
@@ -44,9 +67,38 @@ const Navbar = () => {
               </IconButton>
             </Tooltip>
           </NavLink>
-        </Toolbar>
-      </AppBar>
-    </Box>
+        </Box>
+        <Box sx={{ flexGrow: 0 }}>
+          <Tooltip title="Open settings">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="Remy Sharp" />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {settings.map((setting) => (
+              <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                <Typography textAlign="center">{setting}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
