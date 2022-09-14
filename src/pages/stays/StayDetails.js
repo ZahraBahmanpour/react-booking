8adbdfbb01f348304edbd68ef6978615cf9dac32
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-09-14 12:05:51 +0430

    implement stay details page

--- src/pages/stays/StayDetails.js
+++ src/pages/stays/StayDetails.js
@@ -1,5 +1,20 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import staysServices from "../../services/stays.services";
+
 const StayDetails = () => {
-  return <div>Stay Details</div>;
+  const [stay, setStay] = useState([]);
+  const { stayId } = useParams();
+
+  useEffect(() => {
+    const fetchStay = async () => {
+      const stay = await staysServices.getStayRequest(stayId);
+      setStay(stay);
+    };
+    fetchStay();
+  }, [stayId]);
+
+  return <div>{stay.name}</div>;
 };
 
 export default StayDetails;
