9476ff638cd1dce7ee5ccb368698622ea79f2b46
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-09-17 00:05:35 +0430

    fix typo in city api

--- src/services/city.services.js
+++ src/services/city.services.js
@@ -3,7 +3,7 @@ import axios from "./base.services";
 class CityService {
   getCitiesRequest = async () => {
     try {
-      const response = await axios.get("cities");
+      const response = await axios.get("/cities");
       return response.data;
     } catch (e) {
       return e.message;
