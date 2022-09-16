cb3fb905dc2b25e2a1fbd837ba1dca79f429d8b8
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-09-16 22:34:16 +0430

    add firebase config and errors

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyCBmK5DwSG8N_gNIsBXa8vE-SIAM_iJS0U",
+  authDomain: "react-test-f0083.firebaseapp.com",
+  projectId: "react-test-f0083",
+  storageBucket: "react-test-f0083.appspot.com",
+  messagingSenderId: "6882613416",
+  appId: "1:6882613416:web:69665a9a024b2e4b364a9d",
+  measurementId: "G-7R1T36FDNQ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
---
+++ src/firebase/firebase-errors.js
@@ -0,0 +1,5 @@
+export const USER_NOT_FOUND = "auth/user-not-found";
+export const USER_NOT_FOUND_TRANSLATION = "User Not Found!";
+
+export const WRONG_PASSSWORD = "auth/wrong-password";
+export const WRONG_PASSSWORD_TRANSLATION = "Wrong Password!";
