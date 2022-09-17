ece63dcd4b966627a870a6614ea4dc165c3abf7a
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-09-17 13:13:32 +0430

    add fileupload icon to profile page

--- src/pages/user/UserProfile.js
+++ src/pages/user/UserProfile.js
@@ -1,5 +1,31 @@
+import { Button, Container, IconButton, Stack } from "@mui/material";
+import { UseAuthContext } from "../../context/AuthContext";
+import { AiFillCamera } from "react-icons/ai";
 const UserProfile = () => {
-  return <h1>Profile</h1>;
+  const {
+    user: {
+      userInfo: { displayName },
+    },
+  } = UseAuthContext();
+  return (
+    <Container sx={{ marginTop: 5 }}>
+      <h1>{displayName}</h1>
+      <Stack direction="row" alignItems="center" spacing={2}>
+        <Button variant="contained" component="label">
+          Upload
+          <input hidden accept="image/*" multiple type="file" />
+        </Button>
+        <IconButton
+          color="primary"
+          aria-label="upload picture"
+          component="label"
+        >
+          <input hidden accept="image/*" type="file" />
+          <AiFillCamera />
+        </IconButton>
+      </Stack>
+    </Container>
+  );
 };
 
 export default UserProfile;
