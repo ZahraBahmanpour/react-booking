bada957f46b153e04ff1c81713e03275ebb43c68
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-09-17 12:31:12 +0430

    add create and update favorites service

--- src/services/favorite.services.js
+++ src/services/favorite.services.js
@@ -9,6 +9,25 @@ class FavoriteService {
       return e.message;
     }
   };
+  createFavoritesRequest = async (userId, stayId) => {
+    try {
+      const response = await axios.post("/favorites", {
+        userId,
+        favorites: [stayId],
+      });
+      return response.data;
+    } catch (e) {
+      return e.message;
+    }
+  };
+  updateFavoritesRequest = async (id, favorites) => {
+    try {
+      const response = await axios.patch(`/favorites/${id}`, { favorites });
+      return response.data;
+    } catch (e) {
+      return e.message;
+    }
+  };
 }
 
 export default new FavoriteService();
