88624b63b248c2d0988de7e3dc3c5c9357ba612d
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-09-21 11:11:56 +0430

    add additional filters

---
+++ src/components/FilterSidebar/Filters.js
@@ -0,0 +1,27 @@
+const FilterGroups = [
+  {
+    title: "Budget per night",
+    filters: ["0 - 100", "100 - 200", "200 - 300", "300+"],
+  },
+  {
+    title: "Popular filters",
+    filters: ["Breakfast included", "Pets Allowed", "Parking", "Beach"],
+  },
+  {
+    title: "Fun things to do",
+    filters: [
+      "Hiking",
+      "Cycling",
+      "Fishing",
+      "Horse riding",
+      "Spa",
+      "Indoor pool",
+    ],
+  },
+  {
+    title: "Cancellation policy",
+    filters: ["Free Cancellation", "Book without credit card"],
+  },
+];
+
+export default FilterGroups;
